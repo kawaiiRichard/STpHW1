578ef79e76a07115fada6b1a73fb9eed90686fb5
Kawaii Richard <kawaiirichard@yandex.ru>
2024-03-11 21:31:10 +0300

    4thTask

---
+++ FOurthTaskk/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void fourthTask()
+    {
+        for (int string = 0; string < 3; string++)
+        {
+            for (int column = 0; column < 5; column++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
